aa6c51444accefb9b69774bfbc56c2091ed9f155
choigod1023 <choigod10234@gmail.com>
2018-10-04 21:47:50 +0900

    adsf

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var app = require('express')();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 app.get('/',function(req,res){
     res.sendfile('./public/form.html');
 })
+
+app.post('/',function(req,res){
+    console.log(req.body);
+})
+
 
 app.listen(3000,function(){
     console.log('asdf');
